94eac2502870b83268906d0aea9bb30200fe6269
Kim <kimkl@home.nl>
2026-02-17 09:58:48 +0100

    Improved ceasar cipher
    Now wraps around (from z back to a) and handles user input

---
+++ ceasar-cipher/cc.rb
@@ -0,0 +1,19 @@
+def ceasar_cipher(input, shift_factor)
+
+  input.chars.map do |char|
+    if char.match?(/[a-zA-Z]/)
+      # Decide what base to use, 'a' for lowercase and 'A' for uppercase
+      base = char.downcase == char ? 'a'.ord : 'A'.ord
+      # Calculate the times to shift, modulo 26 for wrap around after z
+      (((char.ord - base + shift_factor) % 26) + base).chr
+    else
+      char
+    end
+  end.join
+end
+
+puts "Enter your text to ceaser_cipher: "
+input = gets.chomp
+puts "Enter the shift factor (an integer!): "
+shift_factor = gets.chomp
+puts ceasar_cipher(input, shift_factor.to_i)
